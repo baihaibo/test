68d0f9410d52ba53667ad4f80825784f59ccabea
bai <bai>
2016-10-10 18:28:38 +0800

    'first'

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+	function downloadandroid(){
+		window.location.href="http://qdzs.jxict.cn/app/qdzs.apk";
+	}
+	function downloadios(){
+		window.location.href="itms-services://?action=download-manifest&url=https://baihaibo.github.io/jxydios/qdzs.plist";
+	}
+	
+
+
