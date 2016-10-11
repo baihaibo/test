fff1d69a3e8aa4c5a6771aa37ae2d32827e121c2
bai <bai>
2016-10-11 18:08:08 +0800

    444

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 	function downloadandroid(){
-		window.location.href="http://qdzs.jxict.cn/app/qdzs.apk";
+		var ua = navigator.userAgent.toLowerCase(),
+        tipsLayer = document.querySelector(".tips");
+        if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+            tipsLayer.style.display = "block";
+        }else{
+			window.location.href="http://qdzs.jxict.cn/app/qdzs.apk";
+		}
 	}
 	function downloadios(){
-		window.location.href="itms-services://?action=download-manifest&url=https://baihaibo.github.io/jxydios/qdzs.plist";
+		var ua = navigator.userAgent.toLowerCase(),
+        tipsLayer = document.querySelector(".tips");
+        if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+            tipsLayer.style.display = "block";
+        }else{
+			window.location.href="itms-services://?action=download-manifest&url=https://baihaibo.github.io/jxydios/qdzs.plist";
+		}	
 	}
 	
 
