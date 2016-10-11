32869a590a870f1ce3ab124b9b9a82a8bce8fb03
bai <bai>
2016-10-11 18:04:00 +0800

    yyrr

--- js/main.js
+++ js/main.js
@@ -2,13 +2,7 @@
 		window.location.href="http://qdzs.jxict.cn/app/qdzs.apk";
 	}
 	function downloadios(){
-		var ua = navigator.userAgent.toLowerCase(),
-        tipsLayer = document.querySelector(".tips");
-        if(ua.match(/MicroMessenger/i) == 'micromessenger'){
-            tipsLayer.style.display = "block";
-        }else{
-			window.location.href="itms-services://?action=download-manifest&url=https://baihaibo.github.io/jxydios/qdzs.plist";
-		}	
+		window.location.href="itms-services://?action=download-manifest&url=https://baihaibo.github.io/jxydios/qdzs.plist";
 	}
 	
 
